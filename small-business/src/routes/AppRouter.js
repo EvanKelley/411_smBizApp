3bbbea48037d3dc386a99820564e59703842e8bb
EvanKelley <sionnach1993@gmail.com>
2023-09-16 16:47:30 -0500

    first push

---
+++ small-business/src/routes/AppRouter.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Listing from '../components/Listing';
+import Details from '../components/Details';
+import Login from '../components/Login';
+import LoggedInListing from '../components/LoggedInListing';
+import AddListing from '../components/AddListing';
+import PrivateRoute from './PrivateRoute';
+
+
+const AppRouter = ({ isAuthenticated }) => {
+  return (
+    <Router>
+      <Switch>
+        {/* Public Routes */}
+        <Route path="/" exact component={Listing} />
+        <Route path="/details/:id" component={Details} />
+        <Route path="/login" component={Login} />
+
+        {/* Protected Routes */}
+        <PrivateRoute
+          path="/loggedinlisting"
+          component={LoggedInListing}
+          isAuthenticated={isAuthenticated}
+        />
+        <PrivateRoute
+          path="/add"
+          component={AddListing}
+          isAuthenticated={isAuthenticated}
+        />
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
---
+++ small-business/src/routes/PrivateRoute.js
@@ -0,0 +1,23 @@
+
+
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+
+
+const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
